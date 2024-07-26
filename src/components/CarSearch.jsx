4ac2629a414e3396db4eb5d7cbc1e1c48335df88
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-07-26 21:27:30 +0530

    Add search form for filtering

--- src/components/CarSearch.jsx
+++ src/components/CarSearch.jsx
@@ -1,3 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeSearchTerm } from "../store";
 export default function CarSearch() {
-  return <h1>CarSearch</h1>;
+  const dispatch = useDispatch();
+  const searchTerm = useSelector((state) => state.cars.searchTerm);
+  const handleSearchTermChange = (e) =>
+    dispatch(changeSearchTerm(e.target.value));
+  return (
+    <div className="list-header">
+      <h3 className="title is-3">My Cars</h3>
+      <div className="search field is-horizontal">
+        <label className="label">Search</label>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+          className="input"
+        />
+      </div>
+    </div>
+  );
 }
