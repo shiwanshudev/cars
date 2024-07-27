f0980c378ea737ac05c792b016c6ca92dd96379d
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-07-27 10:56:22 +0530

    Add car value component

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,3 +1,18 @@
+import { useSelector } from "react-redux";
 export default function CarValue() {
-  return <h1>CarValue</h1>;
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm)
+    );
+    // let cost = 0;
+    // for (let car of filteredCars) {
+    //   cost += car.cost;
+    // }
+    // return cost;
+
+    return filteredCars.reduce((acc, car) => {
+      return acc + car.cost;
+    }, 0);
+  });
+  return <div className="car-value">Total Cost ${totalCost}</div>;
 }
