8f41c4eba3665f59b338fc68c2cafc651c8cfe30
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-07-27 11:23:51 +0530

    Add car highlight when input

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -2,21 +2,36 @@ import { useSelector, useDispatch } from "react-redux";
 import { removeCar } from "../store";
 
 export default function CarList() {
-  const cars = useSelector((state) => state.cars.data);
+  const { cars, name } = useSelector((state) => {
+    const filteredCars = state.cars.data.filter((car) =>
+      car.name.toLowerCase().includes(state.cars.searchTerm)
+    );
+    return {
+      cars: filteredCars,
+      name: state.form.name,
+    };
+  });
+
   const dispatch = useDispatch();
   const handleCarDelete = (car) => {
     dispatch(removeCar(car.id));
   };
-  const renderedCars = cars.map((car) => (
-    <div key={car.id} className="panel">
-      <p>
-        {car.name} - ${car.cost}
-      </p>
-      <button className="button is-danger" onClick={() => handleCarDelete(car)}>
-        Delete
-      </button>
-    </div>
-  ));
+  const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+    return (
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
   return (
     <div className="car-list">
       {renderedCars}
